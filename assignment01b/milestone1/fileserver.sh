e4d8a93f478576fb17821159492580e1a5b997cc
Harsh Arya <harsh.arya@codenation.co.in>
2018-08-07 16:48:34 +0400

    file server

--- assignment01b/milestone1/fileserver.sh
+++ assignment01b/milestone1/fileserver.sh
@@ -1,1 +1,5 @@
- 
+docker volume create servervol
+docker network create --driver=bridge --subnet=172.19.0.0/16 mynet
+cd server && docker build -t assgn1bserver .
+docker run -td -v servervol:/serverdata --network=mynet --ip="172.19.0.3"   assgn1bserver
+
