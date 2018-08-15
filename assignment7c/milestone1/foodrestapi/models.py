db50226df28aabbc37890f94507844bfde052861
Harsh Arya <harsh.arya@codenation.co.in>
2018-08-15 21:26:02 +0400

    price made float

--- assignment7c/milestone1/foodrestapi/models.py
+++ assignment7c/milestone1/foodrestapi/models.py
@@ -33,9 +33,6 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
 
-    def delete(self, using=None, keep_parents=False):
-        self.is_deleted = True
-        self.save()
 
 
 
