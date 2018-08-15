e31b3557df2763ba4b94f1f51e98ac23be1c1a68
Harsh Arya <harsh.arya@codenation.co.in>
2018-08-15 21:19:52 +0400

    price made float

--- assignment7c/milestone1/foodrestapi/models.py
+++ assignment7c/milestone1/foodrestapi/models.py
@@ -42,7 +42,7 @@ class Restaurant(models.Model):
 class Item(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    price = models.PositiveIntegerField()
+    price = models.FloatField(validators=[MinValueValidator(0)])
     restaurant = models.ForeignKey(Restaurant,on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
